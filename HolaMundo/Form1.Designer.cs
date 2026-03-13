769696510c5634eb9f3b1a5a53d07024d7623779
OliverGlez <oliver.gonzalez6314@alumnos.udg.mx>
2026-03-12 23:30:35 -0600

    Agregar archivos de proyecto.

---
+++ HolaMundo/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Contraseña = new System.Windows.Forms.TextBox();
+            this.Confirmar = new System.Windows.Forms.TextBox();
+            this.Validar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.BackColor = System.Drawing.Color.LavenderBlush;
+            this.Contraseña.Location = new System.Drawing.Point(231, 31);
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Size = new System.Drawing.Size(173, 20);
+            this.Contraseña.TabIndex = 0;
+            this.Contraseña.TextChanged += new System.EventHandler(this.Contraseña_TextChanged);
+            // 
+            // Confirmar
+            // 
+            this.Confirmar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.Confirmar.Location = new System.Drawing.Point(231, 94);
+            this.Confirmar.Name = "Confirmar";
+            this.Confirmar.Size = new System.Drawing.Size(173, 20);
+            this.Confirmar.TabIndex = 1;
+            this.Confirmar.TextChanged += new System.EventHandler(this.Confirmar_TextChanged);
+            // 
+            // Validar
+            // 
+            this.Validar.Location = new System.Drawing.Point(231, 163);
+            this.Validar.Name = "Validar";
+            this.Validar.Size = new System.Drawing.Size(173, 47);
+            this.Validar.TabIndex = 2;
+            this.Validar.Text = "Validar";
+            this.Validar.UseVisualStyleBackColor = true;
+            this.Validar.Click += new System.EventHandler(this.Validar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(172, 179);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(451, 241);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Validar);
+            this.Controls.Add(this.Confirmar);
+            this.Controls.Add(this.Contraseña);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Contraseña;
+        private System.Windows.Forms.TextBox Confirmar;
+        private System.Windows.Forms.Button Validar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HolaMundo/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        private const int EM_SETCUEBANNER = 0x1501;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            // 1 para que el texto se mantenga incluso si el foco está en el textbox
+            SendMessage(Contraseña.Handle, EM_SETCUEBANNER, 1, "Escribe tu Contraseña");
+            SendMessage(Confirmar.Handle, EM_SETCUEBANNER, 1, "Confirma tu Contraseña");
+        }
+
+        private void Contraseña_TextChanged(object sender, EventArgs e)
+        {
+            Contraseña.PasswordChar = '*';
+        }
+
+        private void Confirmar_TextChanged(object sender, EventArgs e)
+        {
+            Confirmar.PasswordChar = '*';
+        }
+
+        private void Validar_Click(object sender, EventArgs e)
+        {
+            if (ValidarContrasena(Contraseña.Text))
+            {   // Si la contraseña es válida, comparamos los textos de ambos
+                // textbox para asegurarnos de que coincidan.
+                if (Contraseña.Text == Confirmar.Text)
+                {// Si las contraseñas coinciden, mostramos un mensaje de éxito al usuario.
+                    MessageBox.Show("La contraseña ha sido validada", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {// Si las contraseñas no coinciden, mostramos un mensaje de advertencia al usuario.
+                    MessageBox.Show("Las contraseñas no coinciden.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }// Si la contraseña no cumple con los requisitos, mostramos un mensaje de error al usuario.
+            else
+            {
+                MessageBox.Show("La contraseña no cumple con los requisitos:\n- Al menos una mayúscula\n- Al menos una minúscula\n- Un número\n- Al menos un símbolo especial\n- Mínimo 8 caracteres",
+                                "Validación fallida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidarContrasena(string password)
+        {
+            // Regex: 
+            // (?=.*[a-z]) : Al menos una minúscula
+            // (?=.*[A-Z]) : Al menos una mayúscula
+            // (?=.*\d)    : Al menos un número
+            // (?=.*[^\da-zA-Z]) : Al menos un símbolo (no alfanumérico)
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+    }
+}
